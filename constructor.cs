41d01e2a10b93bc4b66b0b76f47257674b200774
ROHIT SINGH <36737110+funrohit@users.noreply.github.com>
2022-04-09 10:31:39 +0530

    constructor

---
+++ constructor.cs
@@ -0,0 +1,48 @@
+
+namespace Rohit
+{
+class Car
+{
+  public string model;
+
+  
+  public Car(string modelName)
+  {
+    model = modelName;
+  }
+
+  static void Main(string[] args)
+  {
+    Car Ford = new Car("Mustang");
+    Console.WriteLine(Ford.model);
+  }
+}
+
+
+  class Car
+  {
+    public string model;
+    public string color;
+    public int year;
+
+    
+    public Car(string modelName, string modelColor, int modelYear)
+    {
+      model = modelName;
+      color = modelColor;
+      year = modelYear;
+    }
+
+    static void Main(string[] args)
+    {
+      Car Ford = new Car("Mustang", "Red", 1969);
+      Console.WriteLine(Ford.color + " " + Ford.year + " " + Ford.model);
+    }
+  }
+}
+
+
+
+
+
+
