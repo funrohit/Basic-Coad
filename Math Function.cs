169a2c65d34b56825a2d0025829be0484dc9cea4
ROHIT SINGH <36737110+funrohit@users.noreply.github.com>
2022-04-09 10:17:23 +0530

    Math function

---
+++ Math Function.cs
@@ -0,0 +1,51 @@
+namespace Rohit
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(Math.Min(5, 10));  
+     }
+  }
+}
+
+
+
+
+namespace Rohit
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(Math.Sqrt(64));  
+     }
+  }
+}
+
+
+
+
+
+namespace Rohit
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(Math.Abs(-4.7));  
+     }
+  }
+}
+
+
+namespace Rohit
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(Math.Round(9.99));
+     }
+  }
+}
