629f6b496296d705dd1b37e7f329e7d7b45970b3
ROHIT SINGH <36737110+funrohit@users.noreply.github.com>
2022-04-09 10:34:29 +0530

    property of GET SET

---
+++ get set.cs
@@ -0,0 +1,35 @@
+
+namespace Rohit
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Person myObj = new Person();
+      myObj.Name = "Liam";
+      Console.WriteLine(myObj.Name);
+    }
+  }
+}
+
+
+  class Program
+  {  
+    static void Main(string[] args)
+    {
+      Person myObj = new Person();
+      myObj.Name = "Liam";
+      Console.WriteLine(myObj.Name);
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
